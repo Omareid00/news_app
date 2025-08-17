825bb230d78f9aae66c13aa3a1a1b0b4053fecfa
Omar Eid <omereid829@yahoo.com>
2025-08-17 08:37:25 +0300

    dd

---
+++ lib/models/categorie_data_model.dart
@@ -0,0 +1,12 @@
+class categorieDataModel {
+
+  final String categorieId;
+  final String categorieName;
+  final String categorieImage;
+
+  categorieDataModel({
+    required this.categorieId,
+    required this.categorieName,
+    required this.categorieImage,
+  });
+}
